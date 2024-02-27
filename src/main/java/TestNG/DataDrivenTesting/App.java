e76b58df0c92837127e529af5be0384b8bc098fa
adipi10 <adityapithoole10@gmail.com>
2024-02-27 15:45:15 +0530

    first commit

---
+++ src/main/java/TestNG/DataDrivenTesting/App.java
@@ -0,0 +1,13 @@
+package TestNG.DataDrivenTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestNG/DataDrivenTesting/WriteData.java
@@ -0,0 +1,5 @@
+package TestNG.DataDrivenTesting;
+
+public class WriteData {
+
+}
